1699f2b93ae63026ab5150f05c287ce52e7f5444
Pamela Lotriet <pamela.lotriet@gmail.com>
2025-05-08 12:07:46 +0200

    Gallery update

--- README.md
+++ README.md
@@ -57,3 +57,8 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+az acr login --name gwkparty
+docker build --no-cache -t gwkparty.azurecr.io/gwkpartypacks:latest .
+docker push gwkparty.azurecr.io/gwkpartypacks:latest
+az webapp restart --name gwk --resource-group general
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,31 +2,50 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { BlobService } from './blob.service';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [CarouselModule],
+  imports: [CarouselModule, RouterLink],
   templateUrl: './products.component.html',
 })
-export class ProductsComponent implements OnInit {
-  products: any[] = [];
-  blobService = inject(BlobService);
+export class ProductsComponent {
   router = inject(Router);
 
   navigate() {
     this.router.navigate(['home']);
   }
 
-  ngOnInit(): void {
-    this.loadImages();
-  }
-
-  async loadImages() {
-    const urls = await this.blobService.listImageUrls();
-
-    this.products = urls.map((url, index) => ({
-      name: `Image ${index + 1}`,
-      image: url,
-    }));
-  }
+  categories = [
+    {
+      title: 'Party Packs',
+      description:
+        'A custom-designed party pack tailored to your specific request. Typically includes a toy, a packet of chips, a sweet treat, and more. Contact us for special requests!',
+    },
+    {
+      title: 'Chocolate Wrappers',
+      description:
+        'Designed and printed by our team to match your event. Perfect for sweets and chocolates. Prices may vary depending on the item.',
+    },
+    {
+      title: 'Hampers',
+      description:
+        'A beautiful bundle of themed goodies, handpicked for birthdays, baby showers, holidays, or just because. Wrapped with love and bursting with delight.',
+    },
+    {
+      title: 'Embroidery',
+      description:
+        'Personalized embroidery on towels, clothes, bibs, and more. Thoughtful, timeless, and stitched to perfection — one thread at a time.',
+    },
+    {
+      title: 'Special Days',
+      description:
+        "Celebrate life’s little moments — from teacher appreciation gifts to end-of-year tokens, to mother's day gifts. We make the “aww” happen, so you don’t have to.",
+    },
+    {
+      title: 'Party Stickers',
+      description:
+        'Custom-designed stickers for party favors, gift bags, and decor. Stick with us — we’ll make your theme pop like confetti!',
+    },
+  ];
 }
